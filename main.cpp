2a17d409f2e64b4cf7a8a130f6345bf5ade36da8
UjeNeTORT <tikhonovty@gmail.com>
2023-08-18 01:06:23 +0300

    solve now accepts struct equation + enum for number of solutions
    struct equation instead of arrays

--- main.cpp
+++ main.cpp
@@ -2,41 +2,45 @@
 #include <math.h>
 #include <ctype.h>
 
+enum {SOL_0, SOL_1, SOL_2};
+
 const int MISTAKE = 1000;
 const double EPS = 1e-6;
 
-int solve (double *coef, double *sol);
+struct equation {
+    double a;
+    double b;
+    double c;
+    double sol_1;
+    double sol_2;
+} eq = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+int solve (struct equation *eq);
 int is_equal(double a, double b);
 
 int main() {
     int c;
 
-    /* array of a b c coeffs */
-    double coef[3] = {0.0, 0.0, 0.0};
-
-    /* array of solutions */
-    double sol[2] = {0.0, 0.0};
-
     printf("Please, enter a, b, c coefs:\n");
     
     int res;
     int junk;
-    while ((res = scanf("%lf %lf %lf", &coef[0], &coef[1], &coef[2])) != 3) {
+    while ((res = scanf("%lf %lf %lf", &eq.a, &eq.b, &eq.c)) != 3) {
         junk = getchar();
         printf("Incorrect input \"%c\", try again\n", junk);
     }
 
-    switch(c = solve(coef, sol)) {
-        case 0:
+    switch(c = solve(&eq)) {
+        case SOL_0:
             printf("no solutions\n");
             break;
-        case 1: 
+        case SOL_1: 
             printf("1 solution:\n"
-            "%.3lf\n", sol[0]);
+            "%.3lf\n", eq.sol_1);
             break;
-        case 2:
+        case SOL_2:
             printf("2 solutions:\n"
-            "%.3lf , %.3lf\n", sol[0], sol[1]);
+            "%.3lf , %.3lf\n", eq.sol_1, eq.sol_2);
             break;
         default:
             printf("solve returned breeeed\n");
@@ -47,30 +51,30 @@ int main() {
 }
 
 /* returns number of solutions */
-int solve(double *coef, double *sol) {
-    if (is_equal(coef[0], 0.0) == 1) {
+int solve(struct equation *eqp) {
+    if (is_equal((eqp->a), 0.0) == 1) {
         /* x = -c / b*/
-        *sol = ((-1) * coef[2]) / coef[1];
-        return 1;
+        eqp->sol_1 = ((-1) * (eqp->c)) / (eqp->b);
+        return SOL_1;
     }
     
     double d, sd;
 
     /* d = b^2 - 4ac */
-    d = coef[1]*coef[1] - 4*coef[0]*coef[2];
+    d = (eqp->b)*(eqp->b) - 4*(eqp->a)*(eqp->c);
     sd = sqrt(d);
 
     if (d < 0) 
-        return 0;
+        return SOL_0;
     else if (is_equal(d,0.0) == 1) {
-        *sol = -coef[1] / (2 * coef[0]);
-        return 1;
+        eqp->sol_1 = -(eqp->b) / (2 * (eqp->a));
+        return SOL_1;
     }
 
     /* BASED solution v lob */
-    *sol++ = (-coef[1] - sd) / (2 * coef[0]);
-    *sol = (-coef[1] + sd) / (2 * coef[0]);
-    return 2;
+    eqp->sol_1 = (-(eqp->b) - sd) / (2 * (eqp->a));
+    eqp->sol_2 = (-(eqp->b) + sd) / (2 * (eqp->a));
+    return SOL_2;
 }
 
 /* doubles are equal if |a-b| < EPS */
