d80025a83ae041d2d9023becf79193da8b480c25
UjeNeTORT <tikhonovty@gmail.com>
2023-08-22 14:03:58 +0300

    refactor after lection 1

--- square_solver.cpp
+++ square_solver.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
 enum roots_num {
-    ROOTS_NUM_0 = 0, 
-    ROOTS_NUM_1 = 1, 
-    ROOTS_NUM_2 = 2,
-    ROOTS_NUM_INF = 3,
-    ROOTS_NUM_ERR = 4
+    ROOTS_0 = 0, 
+    ROOTS_1 = 1, 
+    ROOTS_2 = 2,
+    ROOTS_INF = 3,
+    ROOTS_ERR = 4
 };
 
 enum error_names {
@@ -17,93 +18,60 @@ enum error_names {
 struct equation {
     double a, b, c; 
     double x1, x2;
-    int number_roots;
+    int n_roots;
 }; 
 
-void solve_input(struct equation *eq);
+void solveInput (struct equation *eq);
 
-void solve_quadr_eq (struct equation *eq);
-void solve_lin_eq (struct equation *eq);
-void solve_inf_roots (struct equation *eq);
-void handle_coef_error (struct equation *eq);
+void solveQuadrEq (const double a, const double b, const double c, double* x1, double* x2, int* n_roots);
+void solveLinEq (const double b, const double c, double* x, int* n_roots);
+void solveInfRoots (int* n_roots);
+void handleCoefError (int* n_roots);
 
-int get_coefs(struct equation *eq);
+int getCoefs (struct equation *eq);
 void print_result(struct equation *eq);
 
-int is_equal(const double a, const double b);
+int isEqual(const double a, const double b);
 
 int main() {
+    printf("# Square equation solver\n"
+           "# (copyright concretno) Yaroslav, 2023\n\n");
 
     struct equation eq = {0.0, 0.0, 0.0, 0.0, 0.0, -1};
 
-    printf("Please, enter a, b, c coefs:\n"); 
+    printf("# Please, enter a, b, c coefs:\n"); 
 
-    int res_get_coefs = -1;
+    int res_getCoefs = getCoefs(&eq); 
 
-    if ((res_get_coefs =  get_coefs(&eq)) == ERR_TOO_MANY_MISTAKES) {
-        printf("too many mistakes\n");
-        return 1;
-    } else if (res_get_coefs == ERR_EOF) {
+    if (res_getCoefs == ERR_EOF) {
         printf("end of the file\n");
         return 1;
-    }   
+    } else if (res_getCoefs == ERR_TOO_MANY_MISTAKES) {
+        printf("too many mistakes\n");
+        return 1;
+    }
     
-
-    solve_input(&eq);
+    solveInput(&eq);
 
     print_result(&eq);
     
     return 0;
 }
 
-void solve_input(struct equation *eq) {
-    if (!is_equal(eq->a, 0.0) && !is_equal(eq->b, 0.0))
-        solve_quadr_eq(eq);
-    else if (!is_equal(eq->b, 0.0))
-        solve_lin_eq(eq);
-    else if (is_equal(eq->c, 0.0))
-        solve_inf_roots(eq);
+void solveInput(struct equation *eq) {
+    if (!isEqual(eq->a, 0.0) && !isEqual(eq->b, 0.0))
+        solveQuadrEq(eq->a, eq->b, eq->c, &eq->x1, &eq->x2, &eq->n_roots);
+    else if (!isEqual(eq->b, 0.0))
+        solveLinEq(eq->b, eq->c, &eq->x1, &eq->n_roots);
+    else if (isEqual(eq->c, 0.0))
+        solveInfRoots(&eq->n_roots);
     else
-        handle_coef_error(eq);
+        handleCoefError(&eq->n_roots);
 }
 
-void solve_quadr_eq(struct equation *eq) {
-    
-    double discriminant = 0.0;
-    double sqrt_discriminant = 0.0;
+int getCoefs(struct equation *eq) {
+    assert (eq);
 
-    /* discriminant = b^2 - 4ac */
-    discriminant = (eq->b) * (eq->b) - 4 * (eq->a) * (eq->c);
-    sqrt_discriminant = sqrt(discriminant);
-
-    if (discriminant < 0) {
-        eq->number_roots = ROOTS_NUM_0;
-    }
-    else if (is_equal(discriminant, 0.0)) {
-        eq->x1 = -(eq->b) / (2 * (eq->a));
-        eq->number_roots = ROOTS_NUM_1;
-    }
-    else {
-        eq->x1 = (-(eq->b) - sqrt_discriminant) / (2 * (eq->a));
-        eq->x2 = (-(eq->b) + sqrt_discriminant) / (2 * (eq->a));
-        eq->number_roots = ROOTS_NUM_2;
-    }
-}
-
-void solve_lin_eq (struct equation *eq) {
-    eq->x1 = -(eq->c) / (eq->b);
-    eq->number_roots = ROOTS_NUM_1;
-}
-
-void solve_inf_roots (struct equation *eq) {
-    eq->number_roots = ROOTS_NUM_INF;
-}
-
-void handle_coef_error (struct equation *eq) {
-    eq->number_roots = ROOTS_NUM_ERR;
-}
-
-int get_coefs(struct equation *eq) {
     static const int MAX_MISTAKES = 10; 
     int garbage = 0, cnt_mistakes = 0;
     while (scanf("%lf %lf %lf", &eq->a, &eq->b, &eq->c) != 3) {
@@ -118,31 +86,79 @@ int get_coefs(struct equation *eq) {
 }
 
 void print_result(struct equation *eq) {
-    switch(eq->number_roots) {
-        case ROOTS_NUM_0:
+    assert (eq);
+
+    switch(eq->n_roots) {
+        case ROOTS_0:
             printf("no solutions\n");
             break;
-        case ROOTS_NUM_1: 
+        case ROOTS_1: 
             printf("1 solution:\n"
                    "%.3lf\n", eq->x1);
             break;
-        case ROOTS_NUM_2:
+        case ROOTS_2:
             printf("2 solutions:\n"
                    "%.3lf , %.3lf\n", eq->x1, eq->x2);
             break;
-        case ROOTS_NUM_INF:
+        case ROOTS_INF:
             printf("infinite solutions\n");
             break;
-        case ROOTS_NUM_ERR:
+        case ROOTS_ERR:
             printf("impossible coefs\n");
             break;
         default:
-            printf("solve_quadr_eq returned breeeed\n");
+            printf("print_result: solveQuadrEq returned breeeed\n");
             break;
     }
 }
 
-int is_equal(const double a, const double b) {
+void solveQuadrEq(const double a, const double b, const double c, double* x1, double* x2, int* n_roots) {
+    assert (isfinite (a));
+    assert (isfinite (b));
+    assert (isfinite (c));
+
+    assert (x1); /* != NULL */
+    assert (x2); /* != NULL */
+    assert (n_roots); /* != NULL */
+    assert (x1 != x2);
+
+    double discriminant = b * b - 4 * a * c;
+    double sqrt_discriminant = sqrt(discriminant);
+
+    if (discriminant < 0) {
+        *n_roots = ROOTS_0;
+    }
+    else if (isEqual(discriminant, 0.0)) {
+        *x1 = *x2 = -b / (2 * a);
+        *n_roots = ROOTS_1;
+    }
+    else {
+        *x1 = (-b - sqrt_discriminant) / (2 * a);
+        *x2 = (-b + sqrt_discriminant) / (2 * a);
+        *n_roots = ROOTS_2;
+    }
+}
+
+void solveLinEq (const double b, const double c, double* x, int* n_roots) {
+    assert (isfinite (b));
+    assert (isfinite (c));
+
+    assert (x);       // != NULL
+    assert (n_roots); // != NULL
+
+    *x = -c / b;
+    *n_roots = ROOTS_1;
+}
+
+void solveInfRoots (int* n_roots) {
+    *n_roots = ROOTS_INF;
+}
+
+void handleCoefError (int* n_roots) {
+    *n_roots = ROOTS_ERR;
+}
+
+int isEqual(const double a, const double b) {
     static const double EPS = 1e-6;
     return (fabs(a-b) < EPS);
 }
