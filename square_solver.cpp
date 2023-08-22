034a4a91ed5cffa99702697a766edfef9f097be9
UjeNeTORT <tikhonovty@gmail.com>
2023-08-22 21:54:01 +0300

    began documentation

--- square_solver.cpp
+++ square_solver.cpp
@@ -18,7 +18,10 @@ enum input_errs {
     ERR_EOF = 2  
 };
 
-
+/**
+ * @brief structure which stores the information about the equation 
+ * 
+*/
 struct equation {
     double a, b, c; 
     double x1, x2;
@@ -95,6 +98,22 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+//-----------------------------------------------------
+/**
+ * @brief given the equation coeffs it decides which function to use to solve the equation
+ * @param [out] eq struct equation, which has all the necessary information about the equation (coefs, roots, number of roots)
+ * 
+ * @details
+ * in equation ax^2 + bx + c = 0 we know a, b, c coeffs (from eq)\n
+ * 
+ * based on what these coeffs are, it solves the equation:\n
+ * 
+ * if (a == 0 && b == 0 && c == 0) calls func solveInfRoots(...)\n
+ * if (a != 0)                     calls func solveQuadrEq(...)\n
+ * if (a != 0 && b != 0)           calls func solveLinEq(...)\n
+ * else                            calls func handleCoefError(...)\n
+*/
+//-----------------------------------------------------
 void solveInput(struct equation *eq) {
     assert (eq);
 
@@ -110,6 +129,23 @@ void solveInput(struct equation *eq) {
         handleCoefError(&eq->n_roots);
 }
 
+//-----------------------------------------------------
+/**
+ * @brief function-tester which tests solveInput function and prints the test result in std output
+ * @details
+ * reads test cases from a file specified in fopen(...)\n
+ * 
+ * initializes struct equation test_eq, later fills it with a, b, c coefs from test cases file\n
+ * initializes struct test_answer test_ans, later fills it with test answers and test number\n
+ * 
+ * test_eq and test_ans are reset to their initial value at the end of each iteration\n
+ * 
+ * defines two variables (tests_passed, n_last_test) which are used later for printing test results\n
+ * if test passed it prints it\n
+ * if test failed it prints what is wrong\n
+ * 
+*/
+//-----------------------------------------------------
 void test_solveInput(void) {
     
     FILE *fp = fopen("test_cases.txt", "r");
@@ -148,6 +184,21 @@ void test_solveInput(void) {
            "##########################\n", tests_passed, n_last_test, (double) tests_passed / n_last_test * 100);
 }
 
+//-----------------------------------------------------
+/**
+ * @brief gets coeffs for quadratic equation from input
+ * @param [out] eq struct equation, which has all the necessary information about the equation (coefs, roots, number of roots)
+ * 
+ * @details
+ * uses scanf to get coefficients from standart input,\n
+ * acanf accepts only 3 double numbers separated by '\n' or ' ' or '\t'
+ * if it cannot scan the numbers       it asks user to try again and increases counter of mistakes\n
+ * if user does too many mistakes      it stops the program by returning enums which stop program in main\n
+ * or if it is the EOF\n                 
+ * if user mistakes during the input   it does not read the rest of the line and asks to try again\n
+ * (for ex. types in a letter)\n
+*/
+//-----------------------------------------------------
 int getCoefs(struct equation *eq) {
     assert (eq);
 
@@ -166,10 +217,24 @@ int getCoefs(struct equation *eq) {
     return 0;
 }
 
+//-----------------------------------------------------
+/**
+ * @brief prints how much solution does the equation eq has ans the roots
+ * @param [out] eq struct equation, which has all the necessary information about the equation (coefs, roots, number of roots)
+ * @details
+ * if 0 roots it prints *no solutions*\n
+ * if 1 root  it prints *1 solution:* and the root\n
+ * if 2 roots it prints *2 solutions:* and the smaller root and then bigger root\n
+ * if roots are infinite it prints *infinite solutions*\n
+*/
+//-----------------------------------------------------
 void print_result(struct equation *eq) {
     assert (eq);
 
     switch(eq->n_roots) {
+        case ROOTS_ERR:
+            printf("no solutions\n");
+            break;
         case ROOTS_0:
             printf("no solutions\n");
             break;
@@ -184,15 +249,28 @@ void print_result(struct equation *eq) {
         case ROOTS_INF:
             printf("infinite solutions\n");
             break;
-        case ROOTS_ERR:
-            printf("impossible coefs\n");
-            break;
         default:
             printf("print_result: solveQuadrEq returned breeeed\n");
             break;
     }
 }
 
+//-----------------------------------------------------
+/**
+ * @brief solves quadratic equation ax^2 + bx + c = 0
+ * @param [in]  a a-coefficient
+ * @param [in]  b b-coefficient
+ * @param [in]  c c-coefficient
+ * @param [out] x1 smaller root
+ * @param [out] x2 bigger root
+ * @param [out] n_roots number of roots of the equation
+ * 
+ * @details
+ * if equation has no roots, n_roots = 0, x1 = 0, x2 = 0\n
+ * if equation has 1 root,   n_roots = 1, x1 = x2 = *root of equation*\n
+ * if equation has 2 roots,  n_roots = 2, x1 = *smaller root*, x2 = *bigger root*
+*/
+//-----------------------------------------------------
 void solveQuadrEq(const double a, const double b, const double c, double *x1, double *x2, int *n_roots) {
     assert (isfinite (a));
     assert (a);
@@ -219,6 +297,14 @@ void solveQuadrEq(const double a, const double b, const double c, double *x1, do
     }
 }
 
+//-----------------------------------------------------
+/**
+ * @brief solves linear equation ax + b = 0 
+ * @param [in] a a-coefficient
+ * @param [in] b b-coefficient
+ * @param [out] x solution of the equation
+*/
+//-----------------------------------------------------
 void solveLinEq (const double a, const double b, double *x, int *n_roots) {
     assert (isfinite (a));
     assert (a);
@@ -231,24 +317,63 @@ void solveLinEq (const double a, const double b, double *x, int *n_roots) {
     *n_roots = ROOTS_1;
 }
 
+//-----------------------------------------------------
+/**
+ * @brief sets n_roots to ROOTS_INF
+ * @param [out] n_roots pointer to int variable
+*/
+//-----------------------------------------------------
 void solveInfRoots (int *n_roots) {
     *n_roots = ROOTS_INF;
 }
 
+//-----------------------------------------------------
+/**
+ * @brief sets n_roots to ROOTS_ERR
+ * @param [out] n_roots pointer to int variable 
+*/
+//-----------------------------------------------------
 void handleCoefError (int *n_roots) {
     *n_roots = ROOTS_ERR;
 }
 
+//-----------------------------------------------------
+/**
+ * @brief resets eq to initial value
+ * @param [out] eq struct equation, which has all the necessary information about the equation (coefs, roots, number of roots)
+*/
+//-----------------------------------------------------
 void resetEq(struct equation *eq) {
     *eq = {0, 0, 0, 0, 0, -1};
 }
 
-
+//-----------------------------------------------------
+/**
+ * @brief resets test_ans to initial value
+ * @param [out] test_ans pointer to struct test_answer variable test_ans
+*/
+//-----------------------------------------------------
 void resetTestAns(struct test_answer *test_ans) {
     *test_ans = {0, 0, 0, 0};
 }
 
+//-----------------------------------------------------
+/**
+ * @brief compares two double values, returns 1 if equal, 0 if not
+ * @param [in] a a-variable
+ * @param [in] b b-variable
+ * 
+ * @details
+ * a == b if |a-b| < EPS,
+ * where EPS is const and defined inside the function
+*/
+//-----------------------------------------------------
 int isEqual(const double a, const double b) {
     static const double EPS = 1e-6;
     return (fabs(a-b) < EPS);
 }
+
+// TODO:
+// - documentation to structs and enums
+// - learn how to output coloured strings
+// 
