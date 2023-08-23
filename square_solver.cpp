b79ebca35f803eb11326cd5b7986320c6b478db7
UjeNeTORT <tikhonovty@gmail.com>
2023-08-23 15:48:40 +0300

    FIXED CODESTYLE, deleted unnecessary stuff

--- square_solver.cpp
+++ square_solver.cpp
@@ -4,96 +4,124 @@
 #include <string.h>
 #include <assert.h>
 
+const double EPS = 1e-6;
 
+//-----------------------------------------------------
+/**
+ * @enum roots_num
+*/
+//-----------------------------------------------------
 enum roots_num {
     ROOTS_0 = 0,
     ROOTS_1 = 1,
     ROOTS_2 = 2,
     ROOTS_INF = 3,
-    ROOTS_ERR = 4
 };
 
+//-----------------------------------------------------
+/**
+ * @enum input_errors
+ * 
+*/
+//-----------------------------------------------------
 enum input_errs {
-    ERR_TOO_MANY_MISTAKES = 1,
+    ERR_OVERFLOW_INPUT = 1,
     ERR_EOF = 2  
 };
 
+//-----------------------------------------------------
 /**
- * @brief structure which stores the information about the equation 
- * 
+ * @struct equation
+ * @brief used to store info about quadratic equation ax^2 + bx + c = 0
 */
+//-----------------------------------------------------
 struct equation {
     double a, b, c; 
     double x1, x2;
-    int n_roots;
+    int nRoots;
 }; 
 
-struct test_answer {
-    int n_test;
+//-----------------------------------------------------
+/**
+ * @struct test_reference
+ * @brief used to store number of the test_case  and answers to it (number of roots of equation and roots themselves x1, x2) 
+*/
+//-----------------------------------------------------
+struct test_reference {
+    int nTest;
     double x1, x2;
-    int n_roots;
+    int nRoots;
 };
 
 
-void solveInput (struct equation *eq);
+void solveSquare (struct equation *eq);
 
-void solveQuadrEq (const double a, const double b, const double c, double *x1, double *x2, int *n_roots);
-void solveLinEq (const double b, const double c, double *x, int *n_roots);
-void solveInfRoots (int *n_roots);
-void handleCoefError (int *n_roots);
+void solveQuadrEq (const double a, const double b, const double c, double *x1, double *x2, int *nRoots);
+void solveLinEq (const double b, const double c, double *x, int *nRoots);
+void solveInfRoots (int *nRoots);
+void handleCoefError (int *nRoots);
 
-void test_solveInput(void);
+void testSolveInput(void);
 
 int getCoefs (struct equation *eq);
-void print_result(struct equation *eq);
+void printResult(struct equation *eq);
+void printInfo(void);
 
 void resetEq(struct equation *eq);
-void resetTestAns(struct test_answer *test_ans);
+void resetTestAns(struct test_reference *testRef);
 
 int isEqual(const double a, const double b);
 
 
+//-----------------------------------------------------
+/**
+ * @brief main-function
+ * @details
+ * depending on what cmd-line arguments are it can:\n
+ * -h           print the header before\n
+ * -test        enables test-mode (runs all the tests from the test_cases file)
+*/
+//-----------------------------------------------------
 int main(int argc, char *argv[]) {
 
-    int test_mode = 0, show_header = 0;
+    int testModeFlag = 0, printInfoFlag = 0;
+    char *flagName;
 
     while (--argc > 0 && **++argv == '-') {
-        char *flag_name = ++*argv;
+        flagName = ++*argv;
 
-        if (strcmp(flag_name, "test") == 0)
-            test_mode = 1;
-        else if (strcmp(flag_name, "h") == 0)
-            show_header = 1;
+        if (strcmp(flagName, "test") == 0)
+            testModeFlag = 1;
+        else if (strcmp(flagName, "h") == 0)
+            printInfoFlag = 1;
         else 
             printf("Invalid arguments\n");
     }
-        
+    
+    if (printInfoFlag)
+        printInfo();
 
-    if (show_header)
-        printf("# Square equation solver\n"
-               "# (copyright concretno) Yaroslav, 2023\n\n");
-
-    if (test_mode) 
-        test_solveInput();
+    if (testModeFlag) resGetCoefs
+        testSolveInput();
     else {
 
         struct equation eq = {0, 0, 0, 0, 0, -1};
 
         printf("# Please, enter a, b, c coefs:\n"); 
 
-        int res_getCoefs = getCoefs(&eq); 
+        int resGetCoefs = getCoefs(&eq); 
 
-        if (res_getCoefs == ERR_EOF) {
+        if (resGetCoefs == ERR_EOF) {
             printf("end of the file\n");
             return 1;
-        } else if (res_getCoefs == ERR_TOO_MANY_MISTAKES) {
+        } else if (resGetCoefs == ERR_OVERFLOW_INPUT) {
             printf("too many mistakes\n");
             return 1;
         }
 
-        solveInput(&eq);
+        solveSquare(&eq);
 
-        print_result(&eq);
+        printResult(&eq);
     }
     return 0;
 }
@@ -109,79 +137,79 @@ int main(int argc, char *argv[]) {
  * based on what these coeffs are, it solves the equation:\n
  * 
  * if (a == 0 && b == 0 && c == 0) calls func solveInfRoots(...)\n
- * if (a != 0)                     calls func solveQuadrEq(...)\n
- * if (a != 0 && b != 0)           calls func solveLinEq(...)\n
+ * else if (a != 0)                calls func solveQuadrEq(...)\n
+ * else if (a != 0 && b != 0)      calls func solveLinEq(...)\n
  * else                            calls func handleCoefError(...)\n
 */
 //-----------------------------------------------------
-void solveInput(struct equation *eq) {
+void solveSquare(struct equation *eq) {
     assert (eq);
 
     if (isEqual(eq->a, 0) && isEqual(eq->b, 0) && isEqual(eq->c, 0)) 
-        solveInfRoots(&eq->n_roots);
+        eq->nRoots = ROOTS_INF;
     else if (!isEqual(eq->a, 0.0)) 
-        solveQuadrEq(eq->a, eq->b, eq->c, &eq->x1, &eq->x2, &eq->n_roots);
+        solveQuadrEq(eq->a, eq->b, eq->c, &eq->x1, &eq->x2, &eq->nRoots);
     else if (!isEqual(eq->b, 0.0)) {
-        solveLinEq(eq->b, eq->c, &eq->x1, &eq->n_roots);
+        solveLinEq(eq->b, eq->c, &eq->x1, &eq->nRoots);
         eq->x2 = eq->x1;
     }
-    else
-        handleCoefError(&eq->n_roots);
+    else 
+        eq->nRoots = ROOTS_0;
 }
 
 //-----------------------------------------------------
 /**
- * @brief function-tester which tests solveInput function and prints the test result in std output
+ * @brief function-tester which tests solveSquare function and prints the test result in std output
  * @details
  * reads test cases from a file specified in fopen(...)\n
  * 
- * initializes struct equation test_eq, later fills it with a, b, c coefs from test cases file\n
- * initializes struct test_answer test_ans, later fills it with test answers and test number\n
+ * initializes struct equation testEq, later fills it with a, b, c coefs from test cases file\n
+ * initializes struct test_reference testRef, later fills it with test answers and test number\n
  * 
- * test_eq and test_ans are reset to their initial value at the end of each iteration\n
+ * testEq and testRef are reset to their initial value at the end of each iteration\n
  * 
- * defines two variables (tests_passed, n_last_test) which are used later for printing test results\n
+ * defines two variables (testsPassed, nLastTest) which are used later for printing test results\n
  * if test passed it prints it\n
  * if test failed it prints what is wrong\n
  * 
 */
 //-----------------------------------------------------
-void test_solveInput(void) {
+void testSolveInput(void) {
     
     FILE *fp = fopen("test_cases.txt", "r");
 
-    struct equation test_eq = {0, 0, 0, 0, 0, -1};
-    struct test_answer test_ans = {0, 0, 0, 0};
+    struct equation testEq = {0, 0, 0, 0, 0, -1};
+    struct test_reference testRef = {0, 0, 0, 0};
     
-    int tests_passed = 0, n_last_test = 0;
+    int testsPassed = 0, nLastTest = 0;
 
     while (fscanf(fp, "%d. %lf %lf %lf | %d %lf %lf\n", 
-                     &test_ans.n_test, &test_eq.a, &test_eq.b, &test_eq.c, &test_ans.n_roots, &test_ans.x1, &test_ans.x2) == 7) {
+                    &testRef.nTest, &testEq.a, &testEq.b, &testEq.c, &testRef.nRoots, &testRef.x1, &testRef.x2) == 7) {
 
-        solveInput(&test_eq);
+        solveSquare(&testEq);
 
-        if (test_eq.n_roots != test_ans.n_roots)
-            printf("Test %d FAILED: wrong n_roots\n", test_ans.n_test);
-        else if (!isEqual(test_eq.x1, test_ans.x1))
-            printf("Test %d FAILED: wrong x1\n", test_ans.n_test);
-        else if (!isEqual(test_eq.x2, test_ans.x2))
-            printf("Test %d FAILED: wrong x2\n", test_ans.n_test);
+        if (testEq.nRoots != testRef.nRoots)
+            printf("Test %d FAILED: wrong nRoots\n", testRef.nTest);
+        else if (!isEqual(testEq.x1, testRef.x1))
+            printf("Test %d FAILED: wrong x1\n", testRef.nTest);
+        else if (!isEqual(testEq.x2, testRef.x2))
+            printf("Test %d FAILED: wrong x2\n", testRef.nTest);
         else {
-            printf("Test %d PASSED\n", test_ans.n_test);
-            tests_passed++;
+            printf("Test %d PASSED\n", testRef.nTest);
+            testsPassed++;
         }
 
-        n_last_test = test_ans.n_test;
+        nLastTest = testRef.nTest;
 
-        resetEq(&test_eq);
-        resetTestAns(&test_ans);
+        resetEq(&testEq);
+        resetTestAns(&testRef);
     }
     fclose(fp);
 
     printf("##########################\n"
            "TESTING FINISHED\n"
            "PASSED [%d/%d] %.lf %%\n"
-           "##########################\n", tests_passed, n_last_test, (double) tests_passed / n_last_test * 100);
+           "##########################\n", testsPassed, nLastTest, (double) testsPassed / nLastTest * 100);
 }
 
 //-----------------------------------------------------
@@ -203,7 +231,7 @@ int getCoefs(struct equation *eq) {
     assert (eq);
 
     static const unsigned MAX_MISTAKES = 10; 
-    unsigned garbage = 0, cnt_mistakes = 0;
+    unsigned garbage = 0, cntWrngLines = 0;
     while (scanf("%lf %lf %lf", &eq->a, &eq->b, &eq->c) != 3) {
         garbage = getchar();
         while (garbage != '\n' && garbage != EOF)
@@ -211,8 +239,8 @@ int getCoefs(struct equation *eq) {
         if (garbage == EOF)
             return ERR_EOF;
         printf("Incorrect input, try again\n");
-        if (cnt_mistakes++ >= MAX_MISTAKES)
-            return ERR_TOO_MANY_MISTAKES;
+        if (cntWrngLines++ >= MAX_MISTAKES)
+            return ERR_OVERFLOW_INPUT;
     }
     return 0;
 }
@@ -222,19 +250,16 @@ int getCoefs(struct equation *eq) {
  * @brief prints how much solution does the equation eq has ans the roots
  * @param [out] eq struct equation, which has all the necessary information about the equation (coefs, roots, number of roots)
  * @details
- * if 0 roots it prints *no solutions*\n
- * if 1 root  it prints *1 solution:* and the root\n
- * if 2 roots it prints *2 solutions:* and the smaller root and then bigger root\n
- * if roots are infinite it prints *infinite solutions*\n
+ * if 0 roots               it prints *no solutions*\n
+ * if 1 root                it prints *1 solution:* and the root\n
+ * if 2 roots               it prints *2 solutions:* and the smaller root and then bigger root\n
+ * if roots are infinite    it prints *infinite solutions*\n
 */
 //-----------------------------------------------------
-void print_result(struct equation *eq) {
+void printResult(struct equation *eq) {
     assert (eq);
 
-    switch(eq->n_roots) {
-        case ROOTS_ERR:
-            printf("no solutions\n");
-            break;
+    switch(eq->nRoots) {
         case ROOTS_0:
             printf("no solutions\n");
             break;
@@ -250,9 +275,19 @@ void print_result(struct equation *eq) {
             printf("infinite solutions\n");
             break;
         default:
-            printf("print_result: solveQuadrEq returned breeeed\n");
+            printf("printResult: solveQuadrEq returned breeeed\n");
             break;
     }
+}
+
+//-----------------------------------------------------
+/**
+ * @brief prints info message: name of the program, author and year before the rest of program
+*/
+//-----------------------------------------------------
+void printInfo(void) {
+    printf("# Square equation solver\n"
+           "# (copyright concretno) Yaroslav, 2023\n\n");
 }
 
 //-----------------------------------------------------
@@ -263,15 +298,15 @@ void print_result(struct equation *eq) {
  * @param [in]  c c-coefficient
  * @param [out] x1 smaller root
  * @param [out] x2 bigger root
- * @param [out] n_roots number of roots of the equation
+ * @param [out] nRoots number of roots of the equation
  * 
  * @details
- * if equation has no roots, n_roots = 0, x1 = 0, x2 = 0\n
- * if equation has 1 root,   n_roots = 1, x1 = x2 = *root of equation*\n
- * if equation has 2 roots,  n_roots = 2, x1 = *smaller root*, x2 = *bigger root*
+ * if equation has no roots, nRoots = 0, x1 = 0, x2 = 0\n
+ * if equation has 1 root,   nRoots = 1, x1 = x2 = *root of equation*\n
+ * if equation has 2 roots,  nRoots = 2, x1 = *smaller root*, x2 = *bigger root*
 */
 //-----------------------------------------------------
-void solveQuadrEq(const double a, const double b, const double c, double *x1, double *x2, int *n_roots) {
+void solveQuadrEq(const double a, const double b, const double c, double *x1, double *x2, int *nRoots) {
     assert (isfinite (a));
     assert (a);
     assert (isfinite (b));
@@ -279,24 +314,25 @@ void solveQuadrEq(const double a, const double b, const double c, double *x1, do
 
     assert (x1); 
     assert (x2); 
-    assert (n_roots); 
+    assert (nRoots); 
     assert (x1 != x2);
 
     double discriminant = b * b - 4 * a * c;
-    double sqrt_discriminant = sqrt(discriminant); 
+    double sqrtDiscriminant = sqrt(discriminant); 
 
     if (discriminant < 0) {
-        *n_roots = ROOTS_0;
+        *nRoots = ROOTS_0;
     } else if (isEqual(discriminant, 0)) {
         *x1 = *x2 = -b / (2 * a);
-        *n_roots = ROOTS_1;
+        *nRoots = ROOTS_1;
     } else {
-        *x1 = (-b - sqrt_discriminant) / (2 * a);
-        *x2 = (-b + sqrt_discriminant) / (2 * a);
-        *n_roots = ROOTS_2;
+        *x1 = (-b - sqrtDiscriminant) / (2 * a);
+        *x2 = (-b + sqrtDiscriminant) / (2 * a);
+        *nRoots = ROOTS_2;
     }
 }
-
+// strerror()
+// errnoelse 
 //-----------------------------------------------------
 /**
  * @brief solves linear equation ax + b = 0 
@@ -305,36 +341,16 @@ void solveQuadrEq(const double a, const double b, const double c, double *x1, do
  * @param [out] x solution of the equation
 */
 //-----------------------------------------------------
-void solveLinEq (const double a, const double b, double *x, int *n_roots) {
+void solveLinEq (const double a, const double b, double *x, int *nRoots) {
     assert (isfinite (a));
     assert (a);
     assert (isfinite (b));
 
     assert (x);      
-    assert (n_roots);
+    assert (nRoots);
 
     *x = -b / a;
-    *n_roots = ROOTS_1;
-}
-
-//-----------------------------------------------------
-/**
- * @brief sets n_roots to ROOTS_INF
- * @param [out] n_roots pointer to int variable
-*/
-//-----------------------------------------------------
-void solveInfRoots (int *n_roots) {
-    *n_roots = ROOTS_INF;
-}
-
-//-----------------------------------------------------
-/**
- * @brief sets n_roots to ROOTS_ERR
- * @param [out] n_roots pointer to int variable 
-*/
-//-----------------------------------------------------
-void handleCoefError (int *n_roots) {
-    *n_roots = ROOTS_ERR;
+    *nRoots = ROOTS_1;
 }
 
 //-----------------------------------------------------
@@ -349,12 +365,12 @@ void resetEq(struct equation *eq) {
 
 //-----------------------------------------------------
 /**
- * @brief resets test_ans to initial value
- * @param [out] test_ans pointer to struct test_answer variable test_ans
+ * @brief resets testRef to initial value
+ * @param [out] testRef pointer to struct test_reference variable testRef
 */
 //-----------------------------------------------------
-void resetTestAns(struct test_answer *test_ans) {
-    *test_ans = {0, 0, 0, 0};
+void resetTestAns(struct test_reference *testRef) {
+    *testRef = {0, 0, 0, 0};
 }
 
 //-----------------------------------------------------
@@ -365,11 +381,10 @@ void resetTestAns(struct test_answer *test_ans) {
  * 
  * @details
  * a == b if |a-b| < EPS,
- * where EPS is const and defined inside the function
+ * where EPS is const and defined outside the function
 */
 //-----------------------------------------------------
 int isEqual(const double a, const double b) {
-    static const double EPS = 1e-6;
     return (fabs(a-b) < EPS);
 }
 
